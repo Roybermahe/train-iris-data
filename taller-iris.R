2441de433179ede58a3e2093d28fdfa93f4238d2
Royber <roybermanjarrez@gmail.com>
2021-03-09 15:30:17 -0500

    update taller-iris.rmd

--- taller-iris.R
+++ taller-iris.R
@@ -2,7 +2,7 @@
 # Objective : TODO
 # Created by: royber manjarrez y gustavo
 # Created on: 8 mar. 2021
-irisData <- read.csv("C:/users/royber manjarrez/Downloads/iris.data", header = FALSE, stringsAsFactors = TRUE)
+irisData <- read.csv("https://raw.githubusercontent.com/Roybermahe/train-with-iris/main/iris%20(3).data", header = FALSE, stringsAsFactors = TRUE)
 names(irisData) <- c("Sepal.length", "Sepal.width","Petal.length","Petal.width", "Species")
 summary(irisData)
 
